9544d13030a139e604206f04dfbfc8c3f5410ac4
Daniel Efrén <deduefren@gmail.com>
2021-11-05 09:17:07 +0100

    Test

--- api/PostContactForm.cs
+++ api/PostContactForm.cs
@@ -15,7 +15,7 @@ namespace deduefrencv.postcontactform
     {
         [FunctionName("PostContactForm")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             [SendGrid(ApiKey = "AzureWebJobsSendGridApiKey")] IAsyncCollector<SendGridMessage> messageCollector,
             ILogger log)
         {
