2f7c3a7aafb610c59208da1087be7f3099aa66e2
Daniel Efrén <deduefren@gmail.com>
2021-11-11 16:41:06 +0100

    Fix

--- cv-app/src/app/app.component.ts
+++ cv-app/src/app/app.component.ts
@@ -11,15 +11,17 @@ export class AppComponent {
 
   constructor(translate: TranslateService) {
 
-    if (translate != undefined){
-      // this language will be used as a fallback when a translation isn't found in the current language
-      translate.setDefaultLang('en');
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('en');
 
-      var browserLang = translate.getBrowserCultureLang().substr(0,2);
+    var browserLang = translate.getBrowserCultureLang();
+    if (browserLang != undefined)
+    {
+      var lang = browserLang.substr(0,2);
       // the lang to use, if the lang isn't available, it will use the current loader to get them
       //translate.use('es');
       //If it's supported use it, if not, we are going to english by default
-      if (browserLang == 'es' || browserLang == 'en')
+      if (lang == 'es' || lang == 'en')
       {
         translate.use(browserLang);
       }
