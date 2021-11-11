914eeb61fc49964cec3f09321ed49e071471eaac
Daniel Efrén <deduefren@gmail.com>
2021-11-11 16:35:34 +0100

    Fix language selecion.

--- cv-app/src/app/app.component.ts
+++ cv-app/src/app/app.component.ts
@@ -11,12 +11,18 @@ export class AppComponent {
 
   constructor(translate: TranslateService) {
 
+    if (translate != undefined){
+      // this language will be used as a fallback when a translation isn't found in the current language
+      translate.setDefaultLang('en');
 
-    // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
-
-     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    //translate.use('es');
-    translate.use(translate.getBrowserCultureLang().substr(0,2));
+      var browserLang = translate.getBrowserCultureLang().substr(0,2);
+      // the lang to use, if the lang isn't available, it will use the current loader to get them
+      //translate.use('es');
+      //If it's supported use it, if not, we are going to english by default
+      if (browserLang == 'es' || browserLang == 'en')
+      {
+        translate.use(browserLang);
+      }
+    }
   }
 }
