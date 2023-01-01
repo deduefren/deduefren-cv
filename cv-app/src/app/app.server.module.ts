f2eceebaec4319c930354d8bcd409cd94be09137
Daniel Efrén Díaz Ureta <deduefren@gmail.com>
2023-01-01 13:12:52 +0100

    Fix translations

--- cv-app/src/app/app.server.module.ts
+++ cv-app/src/app/app.server.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
+import {
+  ServerModule,
+  ServerTransferStateModule,
+} from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TransferState } from '@angular/platform-browser';
+import { translateServerLoaderFactory } from 'src/loaders/translate-server.loader';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    ServerTransferStateModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateServerLoaderFactory,
+        deps: [TransferState],
+      },
+    }),
   ],
   bootstrap: [AppComponent],
 })
